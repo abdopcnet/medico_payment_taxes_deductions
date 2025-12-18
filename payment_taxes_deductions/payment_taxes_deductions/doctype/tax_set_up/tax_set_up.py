98d6d09885752e06c7a60e953951e977fd6a2458
abdopcnet <abdopcnet@gmail.com>
2025-12-18 16:45:22 +0200

    ❌ tax_set_up.py

--- payment_taxes_deductions/payment_taxes_deductions/doctype/tax_set_up/tax_set_up.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, abdopcnet@gmail.com and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class TaxSetUp(Document):
-	pass
